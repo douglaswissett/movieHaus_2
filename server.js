74b7dfcf419d909e3d16f62a64b52721c2a7b3e7
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-03-05 16:18:52 -0500

    server init

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict'
+var express = require('express');
+var logger  = require('morgan');
+var path    = require('path');
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// homepage
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+// theatres route
+app.get('/theatres', (req, res) => {
+  // takes db.showThreatres
+  //res.send(res.rows);
+});
+
+app.get('/theatres/:id', (req, res) => {
+  // takes db.showTheatreMovies
+  //res.send(res.rows);
+});
+
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('listening on ', port);
+});
