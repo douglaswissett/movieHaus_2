cfd76a88a4eb19e55a3062dcc2bdc6a462f01a8c
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-03-05 16:23:08 -0500

    fixed index route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // homepage
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // theatres route
