e92d50de3e9cb2a53a30082f5035727d34723001
penguyen <peternguyen008@gmail.com>
2016-03-05 18:58:17 -0500

    completed all pgp functions besides addMovie

--- db/pgp.js
+++ db/pgp.js
@@ -26,9 +26,9 @@ function showTheatres(req,res,next){
 function showTheatreMovie(req,res,next){
   // var tID = req.params.id;
   db.many("SELECT * FROM movies m " + 
-          "INNER JOIN theatres_movies_join tmj " + 
-          "ON m.movie_id = tmj.movie_id " + 
-          "WHERE tmj.theatre_id = ($1);", [req.params.id])
+          "INNER JOIN theatre_movie_showtime tms " + 
+          "ON m.movie_id = tms.movie_id " + 
+          "WHERE tms.theatre_id = ($1);", [req.params.id])
     .then(function(data){
       console.log(data);  // should get multiple movies in a theatre
       res.rows = data;
@@ -55,30 +55,36 @@ function getMovie(req,res,next){
 
 // 1- add movie to movies table 
 // 2- get last movie_id
-// 3- (iterate) add actor_name to actors table 
-// 3a- get last actor_id
-// 3b- add last actor_id & last movie_id to movies_actors_table 
-// 4- add movie_id & theatre_id to theatres_movies_join table
+// 3- (iterate) add each showTime to theatre_movie_showtime WHERE movie_id AND theatre_id
 function addMovie(req,res,next){}
 
 // edit movie_id info in movies table
 function editMovie(req,res,next){
   // var mID = req.params.id;
   db.none("UPDATE movies SET title=($1), year=($2), rating=($3), director=($4), " +
-          "actors=($5), plot=($6), showTimes=($7) WHERE movie_id=($8);",
-          [req.body.title, req.body.year, req.body.rating, req.body.director, req.body.actors, req.body.plot, req.body.showTimes, req.params.id])
-    // how to update actors??? since its an array of actors 
+          "plot=($5), actors=($6) WHERE movie_id=($7);",
+          [req.body.title, req.body.year, req.body.rating, req.body.director, req.body.plot, req.body.actors, req.params.id])
     .then(function(){
-
+      console.log('UPDATE COMPLETED!');     // testing status for UPDATE
+      next();
     })
     .catch(function(){
-
+      console.log('ERROR in editing MOVIE DETAILS!');
     })
 }
 
-// delete from theatres_movies_join where movie_id=($1) and theatre_id=($2)
+// delete from theatre_movie_showtime where movie_id=($1) and theatre_id=($2);
 function deleteMovie(req,res,next){
   // var mID = req.params.id;
+  db.none("DELETE FROM theatre_movie_showtime WHERE movie_id=($1) AND theatre_id=($2);",
+          [req.params.mID, req.params.tID])   // how do we differentiate btw movie_id & theatre_id ??
+    .then(function(){
+      console.log('DELETE COMPLETED!');     // testing status for DELETE
+      next();
+    })
+    .catch(function(){
+      console.log('ERROR in getting MOVIE DETAILS!');
+    })
 }
 
 module.exports.showTheatres = showTheatres;
@@ -87,3 +93,7 @@ module.exports.getMovie = getMovie;
 module.exports.addMovie = addMovie;
 module.exports.editMovie = editMovie;
 module.exports.deleteMovie = deleteMovie;
+
+
+
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
 DROP TABLE if exists theatres CASCADE;
 DROP TABLE if exists movies CASCADE;
-DROP TABLE if exists actors CASCADE;
-DROP TABLE if exists theatres_movies_join CASCADE;
-DROP TABLE if exists movies_actors_join CASCADE;
+DROP TABLE if exists theatre_movie_showtime CASCADE;
 
 CREATE TABLE theatres (   
   theatre_id serial PRIMARY KEY UNIQUE,
@@ -11,29 +9,25 @@ CREATE TABLE theatres (
 
 CREATE TABLE movies (   
   movie_id serial PRIMARY KEY UNIQUE,
-  name VARCHAR(255),
-  img_url VARCHAR(255),
   title VARCHAR(255),
+  img_url VARCHAR(255),
   year int,
   rating numeric,
   director VARCHAR(255),
   plot VARCHAR(255),
-  showTimes VARCHAR(255)
+  actors VARCHAR(255)
 );
 
-CREATE TABLE actors (   
-  actor_id serial PRIMARY KEY UNIQUE,
-  name VARCHAR(255)
-);
-
-CREATE TABLE theatres_movies_join (
+-- showTime for each movie_theatre pair
+CREATE TABLE theatre_movie_showtime (
+  showTime_id serial PRIMARY KEY UNIQUE,
+  showTime timestamp,
   theatre_id integer REFERENCES theatres ON DELETE CASCADE,
-  movie_id integer REFERENCES movies ON DELETE CASCADE,
-  PRIMARY KEY (theatre_id, movie_id)
+  movie_id integer REFERENCES movies ON DELETE CASCADE
 );
 
-CREATE TABLE movies_actors_join (
-  movie_id integer REFERENCES movies ON DELETE CASCADE,
-  actor_id integer REFERENCES actors ON DELETE CASCADE,
-  PRIMARY KEY (movie_id, actor_id)
-);
+-- CREATE TABLE theatres_movies_join (
+--   theatre_id integer REFERENCES theatres ON DELETE CASCADE,
+--   movie_id integer REFERENCES movies ON DELETE CASCADE,
+--   PRIMARY KEY (theatre_id, movie_id)
+-- );
